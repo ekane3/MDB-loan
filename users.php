b7dd5a0d2fb16c5580f9a2dc546258c2709d1316
EKANE <ekaneekane3@gmail.com>
2022-06-27 08:19:47 +0200

    [UPDATE] exports buttons added

--- users.php
+++ users.php
@@ -20,6 +20,7 @@
   <link href="css/mdb.min.css" rel="stylesheet">
   <!--Datatable css-->
   <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css" rel="stylesheet">
 
 
 </head>
@@ -275,6 +276,11 @@
       <!--Datatables-->
     <script  src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script  src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script  src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script  src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 
     <script type="text/javascript">
         /* WOW.js init */
@@ -306,7 +312,15 @@
         */
         $(document).ready(function () {
 
-            $('.table').DataTable();
+            $('.table').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    //'copyHtml5',
+                    //'excelHtml5',
+                    //'csvHtml5',
+                    'pdfHtml5'
+                ]
+            });
             // edit button
             $(document).on("click","#edit", function() {
            // $('#edit').click( function(){
